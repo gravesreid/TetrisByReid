6722fbf524c4717736e42e11c0947dfa8c9b9c7c
reid <gravesreid@gmail.com>
2025-11-09 17:02:40 -0500

    updated colors

--- build/fssimplewindow_build/ysbitmapfont/CMakeFiles/ysglbitmapfont.dir/src/ysglfontdata.c.o.d
+++
@@ -1,3 +0,0 @@
-fssimplewindow_build/ysbitmapfont/CMakeFiles/ysglbitmapfont.dir/src/ysglfontdata.c.o: \
- /home/reid/projects/engineering_computation/great_YS_libraries/fssimplewindow/ysbitmapfont/src/ysglfontdata.c \
- /usr/include/stdc-predef.h
--- build/yssimplesound_build/CMakeFiles/yssimplesound.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for yssimplesound.
-# This may be replaced when dependencies are built.
